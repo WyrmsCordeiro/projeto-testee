e76a417f3b1ba3635b44817ca4efd30f35ac77ac
WyrmsCordeiro <wyrmsc@gmail.com>
2025-10-25 14:47:48 +0000

    Criei códigos de algoritmo

--- back-end/algoritmo.java
+++ back-end/algoritmo.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
+
+public class Soma {
+    public static void main (String [] args) {
+        Scanner scanner = new Scanner(System.in);
+    
+    double queijo = 7.0;
+    double pao = 10.00;
+    double presunto = 8.00;
+
+    double valorTotal = queijo + pao + presunto;
+    System.out.println("O valor da compra no supermercado foi: " + valorTotal);
+    
+    scanner.close();
+    
+    
+    }
+}
 
